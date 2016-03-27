96ea845d19a9e343838a3fa51bfadbbfc4b2ce0c
Alessio Linares <alessio@alessio.cc>
2016-03-27 17:44:49 +0200

    Properties added

---
+++ include/tiled/Properties.hpp
@@ -0,0 +1,25 @@
+#ifndef TILED_PROPERTIES_HPP
+#define TILED_PROPERTIES_HPP
+#include <unordered_map>
+#include <string>
+#include "Value.hpp"
+
+namespace tiled
+{
+class Properties
+{
+public:
+    virtual ~Properties();
+    void set(const std::string& name, bool value);
+    void set(const std::string& name, int value);
+    void set(const std::string& name, float value);
+    void set(const std::string& name, const std::string& value);
+    void set(const std::string& name, const char* value);
+    const Value* get(const std::string& name) const;
+    void clear();
+
+private:
+    std::unordered_map<std::string, Value*> p_bag;
+};
+} /* tiled */
+#endif /* ifndef TILED_PROPERTIES_HPP */
